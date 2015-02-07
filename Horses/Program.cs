45b4adf6515b7bac3ac0fd2f26f8d5bf869dce11
Lucas Cullen <support@dugongsoftware.com>
2015-02-07 01:28:43 -0800

    Add time parser

--- Horses/Program.cs
+++ Horses/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
@@ -12,6 +14,8 @@ namespace Horses
     {
         static void Main(string[] args)
         {
+            GetTimes();
+
             HorseEntities db = new HorseEntities();
 
             //Give horses an arbitary number
@@ -20,128 +24,136 @@ namespace Horses
             //counter
             //Int32 horseCounter = 0;
 
-            DateTime startDate = new DateTime(2011, 1, 1);
+            DateTime startDate = new DateTime(2015, 2, 7);
 
             while (startDate < System.DateTime.Now)
             {
-                XDocument raceDay = new XDocument();
-                raceDay = XDocument.Load(String.Format("https://tatts.com/pagedata/racing/{0}/{1}/{2}/RaceDay.xml", startDate.Year, startDate.Month, startDate.Day));
 
-                IEnumerable<XElement> meetings = from el in raceDay.Descendants("Meeting")
-                                                 select el;
-
-                foreach (XElement meet in meetings)
+                try
                 {
-                    String venueName = meet.Attribute("VenueName").Value;
-                    
+                    XDocument raceDay = XDocument.Load(String.Format("https://tatts.com/pagedata/racing/{0}/{1}/{2}/RaceDay.xml", startDate.Year, startDate.Month, startDate.Day));
 
-                    Track track = db.Tracks.FirstOrDefault(t => t.Name == venueName);
-                    if (track != null)
+                    IEnumerable<XElement> meetings = from el in raceDay.Descendants("Meeting")
+                                                     select el;
+
+                    foreach (XElement meet in meetings)
                     {
-                        String meetingCode = meet.Attribute("MeetingCode").Value;
-                        Int16 numberOfRaces = Convert.ToInt16(meet.Attribute("HiRaceNo").Value);
+                        String venueName = meet.Attribute("VenueName").Value;
 
-                        for (Int16 i = 1; i <= numberOfRaces; i++)
+
+                        Track track = db.Tracks.FirstOrDefault(t => t.Name == venueName);
+                        if (track != null)
                         {
-                            Console.WriteLine("{0} R{2} on {1}", venueName, startDate, i);
+                            String meetingCode = meet.Attribute("MeetingCode").Value;
+                            Int16 numberOfRaces = Convert.ToInt16(meet.Attribute("HiRaceNo").Value);
 
-                            try
+                            for (Int16 i = 1; i <= numberOfRaces; i++)
                             {
-                                XDocument data = XDocument.Load(String.Format("https://tatts.com/pagedata/racing/{0}/{1}/{2}/{3}{4}.xml", startDate.Year, startDate.Month, startDate.Day, meetingCode, i));
-                                //XElement data = XElement.Load(String.Format("https://tatts.com/pagedata/racing/{0}/{1}/{2}/{3}{4}.xml", startDate.Year, startDate.Month, startDate.Day, meetingCode, i));
+                                Console.WriteLine("{0} R{2} on {1}", venueName, startDate, i);
 
-                                //Get races
-                                IEnumerable<XElement> raceElements = from x in data.Descendants("Race")
-                                                          select x;
-
-                                foreach (XElement raceElement in raceElements)
+                                try
                                 {
-                                    try
+                                    XDocument data = XDocument.Load(String.Format("https://tatts.com/pagedata/racing/{0}/{1}/{2}/{3}{4}.xml", startDate.Year, startDate.Month, startDate.Day, meetingCode, i));
+                                    //XElement data = XElement.Load(String.Format("https://tatts.com/pagedata/racing/{0}/{1}/{2}/{3}{4}.xml", startDate.Year, startDate.Month, startDate.Day, meetingCode, i));
+
+                                    //Get races
+                                    IEnumerable<XElement> raceElements = from x in data.Descendants("Race")
+                                                                         select x;
+
+                                    foreach (XElement raceElement in raceElements)
                                     {
-                                        //XElement winner = (from el in raceElement.Descendants("ResultPlace")
-                                        //                   where (string)el.Attribute("PlaceNo") == "1"
-                                        //                   select el).First();
-
-                                        //String winnerNumber = (from el in winner.Descendants("Result")
-                                        //                       select el.Attribute("RunnerNo").Value).First();
-
-
-                                        Race race = new Race()
+                                        try
                                         {
-                                            RaceNumber = Convert.ToInt32(raceElement.Attribute("RaceNo").Value),
-                                            Distance = Convert.ToInt32(raceElement.Attribute("Distance").Value),
-                                            TrackCondition = Convert.ToInt16(raceElement.Attribute("TrackCond").Value),
-                                            WeatherCondition = Convert.ToInt16(raceElement.Attribute("WeatherCond").Value),
-                                            Start = DateTime.Parse(raceElement.Attribute("RaceTime").Value),
-                                            TrackID = track.ID
-                                        };
+                                            //XElement winner = (from el in raceElement.Descendants("ResultPlace")
+                                            //                   where (string)el.Attribute("PlaceNo") == "1"
+                                            //                   select el).First();
 
-                                        db.Races.Add(race);
+                                            //String winnerNumber = (from el in winner.Descendants("Result")
+                                            //                       select el.Attribute("RunnerNo").Value).First();
 
-                                        IEnumerable<XElement> runnerElements = from el in raceElement.Descendants("Runner")
-                                                                        select el;
 
-                                        foreach (XElement runnerElement in runnerElements)
-                                        {
+                                            Race race = new Race()
+                                            {
+                                                RaceNumber = Convert.ToInt32(raceElement.Attribute("RaceNo").Value),
+                                                Distance = Convert.ToInt32(raceElement.Attribute("Distance").Value),
+                                                TrackCondition = Convert.ToInt16(raceElement.Attribute("TrackCond").Value),
+                                                WeatherCondition = Convert.ToInt16(raceElement.Attribute("WeatherCond").Value),
+                                                Start = DateTime.Parse(raceElement.Attribute("RaceTime").Value),
+                                                TrackID = track.ID
+                                            };
+
+                                            db.Races.Add(race);
+
+                                            IEnumerable<XElement> runnerElements = from el in raceElement.Descendants("Runner")
+                                                                                   select el;
+
+                                            foreach (XElement runnerElement in runnerElements)
+                                            {
+                                                try
+                                                {
+                                                    Horse horse = new Horse(); // GetHorse(runnerElement);
+                                                    Runner runner = new Runner();
+                                                    runner.Race = race;
+
+                                                    String r = runnerElement.Attribute("RunnerName").Value;
+
+                                                    Horse dbHorse = GetHorse(r);
+                                                    runner.HorseName = dbHorse.Name;
+                                                    runner.HorseID = dbHorse.ID;
+                                                    runner.Barrier = Convert.ToInt16(runnerElement.Attribute("Barrier").Value);
+
+                                                    //    horse.RunnerName = runner.Attribute("RunnerName").Value;
+                                                    //    horse.Rider = runner.Attribute("Rider").Value;
+                                                    //    horse.RunnerNo = Convert.ToInt16(runner.Attribute("RunnerNo").Value);
+
+                                                    runner.Weight = Convert.ToDecimal(runnerElement.Attribute("Weight").Value);
+
+                                                    //    if (horse.RunnerNo.ToString() == winnerNumber)
+                                                    //    {
+                                                    //        horse.Won = true;
+                                                    //    }
+
+                                                    Console.WriteLine("Addding {0}", runner.HorseName);
+
+                                                    db.Runners.Add(runner);
+
+                                                }
+                                                catch (Exception ex)
+                                                {
+                                                    Console.WriteLine(ex.Message);
+                                                }
+                                            }
+
+                                            Console.WriteLine("Adding race {0}", race.RaceNumber);
+
                                             try
                                             {
-                                                Horse horse = new Horse(); // GetHorse(runnerElement);
-                                                Runner runner = new Runner();
-                                                runner.Race = race;
 
-                                                String r = runnerElement.Attribute("RunnerName").Value;
-
-                                                Horse dbHorse = GetHorse(r);
-                                                runner.HorseName = dbHorse.Name;
-                                                runner.HorseID = dbHorse.ID;
-                                                runner.Barrier = Convert.ToInt16(runnerElement.Attribute("Barrier").Value);
-
-                                                //    horse.RunnerName = runner.Attribute("RunnerName").Value;
-                                                //    horse.Rider = runner.Attribute("Rider").Value;
-                                                //    horse.RunnerNo = Convert.ToInt16(runner.Attribute("RunnerNo").Value);
-
-                                                runner.Weight = Convert.ToDecimal(runnerElement.Attribute("Weight").Value);
-
-                                                //    if (horse.RunnerNo.ToString() == winnerNumber)
-                                                //    {
-                                                //        horse.Won = true;
-                                                //    }
-
-                                                Console.WriteLine("Addding {0}", runner.HorseName);
-
-                                                db.Runners.Add(runner);
-
+                                                db.SaveChanges();
                                             }
                                             catch (Exception ex)
                                             {
                                                 Console.WriteLine(ex.Message);
                                             }
                                         }
-
-                                        Console.WriteLine("Adding race {0}", race.RaceNumber);
-
-                                        try
-                                        {
-                                            
-                                            db.SaveChanges();
-                                        }
                                         catch (Exception ex)
                                         {
                                             Console.WriteLine(ex.Message);
                                         }
                                     }
-                                    catch (Exception ex)
-                                    {
-                                        Console.WriteLine(ex.Message);
-                                    }
                                 }
-                            }
-                            catch (Exception ex)
-                            {
-                                Console.WriteLine(ex.Message);
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine(ex.Message);
+                                }
                             }
                         }
                     }
+                }
+                catch (Exception ex)
+                {
+                    //no xml
+                    Console.WriteLine(ex.Message);
                 }
 
                 startDate = startDate.AddDays(1);
@@ -179,6 +191,21 @@ namespace Horses
                 {
                     return horse;
                 }
+            }
+        }
+
+        private static void GetTimes()
+        {
+            WebClient client = new WebClient();
+            String html = client.DownloadString("http://www.racingandsports.com.au/en/form-guide/fullresults.asp?meeting=54007&date=7/02/2015");
+
+            Regex timeRex = new Regex("[0-2]{1}:[0-9]{2}.[0-9]{2}");
+
+            MatchCollection times = timeRex.Matches(html);
+
+            foreach(Match time in times)
+            {
+                Console.WriteLine(time.Value);
             }
         }
     }
